ce5ecb3bfd6ef4a59cef433ed183a83a3d8a63e0
dahalh <dahalh@outlook.com>
2022-02-09 13:42:24 +1100

    condition functional

--- script.js
+++ script.js
@@ -17,38 +17,27 @@ const calculate = () => {
     
     // Input is string so typecasting is necessary. */
     let totalMark =
-        // parseFloat(grade1) +
-        // parseFloat(grade2) +
-        // parseFloat(grade3) +
-        // parseFloat(grade4) +
-        // parseFloat(grade5);
-        ((parseFloat(grade1)) * (parseFloat(weighing1)/100)) + ((parseFloat(grade2)) * (parseFloat(weighing2)/100)) + ((parseFloat(grade3)) * (parseFloat(weighing3)/100)) + ((parseFloat(grade4)) * (parseFloat(weighing4)/100)) + ((parseFloat(grade5)) * (parseFloat(weighing5)/100));
+        
+        Math.floor(((parseFloat(grade1)) * (parseFloat(weighing1)/100)) + ((parseFloat(grade2)) * (parseFloat(weighing2)/100)) + ((parseFloat(grade3)) * (parseFloat(weighing3)/100)) + ((parseFloat(grade4)) * (parseFloat(weighing4)/100)) + ((parseFloat(grade5)) * (parseFloat(weighing5)/100)));
     
     // Checking the condition for the providing the
     // grade to student based on percentage
-    // let percentage = (totalgrades / 500) * 100;
-    // if (percentage <= 100 && percentage >= 80) {
-    //     grades = "A";
-    // } else if (percentage <= 79 && percentage >= 60) {
-    //     grades = "B";
-    // } else if (percentage <= 59 && percentage >= 40) {
-    //     grades = "C";
-    // } else {
-    //     grades = "F";
-    // }
-    if (totalMark <= 100 || totalMark >= 85) {
-        grades = "HD";
-    } else if (totalMark <= 85 || totalMark >= 75) {
-        grades = "D";
-    } else if (totalMark <= 74 || totalMark >= 65) {
-        grades = "C";
-    } else if (totalMark <= 64 || totalMark >= 50 ) {
-        grades = "P";
+
+
+
+    if (totalMark <= 100 && totalMark >= 85) {
+        grades = "High Distinction";
+    } else if (totalMark <= 85 && totalMark >= 75) {
+        grades = "Distinction";
+    } else if (totalMark <= 74 && totalMark >= 65) {
+        grades = "Credit";
+    } else if (totalMark <= 64 && totalMark >= 50 ) {
+        grades = "Pass";
     } else {
-        grades = "F";
+        grades = "Fail";
     }
-    // Checking the values are empty if empty than
-    // show please fill them
+
+    // Checking if all values are filled
     if (grade1 == "" || grade2 == ""
                 || grade3 == "" || grade4 == "" || grade5 == "" || weighing1 == "" || weighing2 == "" || weighing3 == "" || weighing4 == "" || weighing5 == "") {
         document.querySelector("#showdata").innerHTML
@@ -61,14 +50,12 @@ const calculate = () => {
             document.querySelector(
                 "#showdata"
             ).innerHTML = 
-            `Your final mark is ${totalMark}. You have passed.`
-            // `Your final mark is ${totalMark} and your grade is ${grades}.`
+            `Your final mark is ${totalMark} and your grade is ${grades}.`
         } else {
             document.querySelector(
                 "#showdata"
             ).innerHTML = 
-            `Your final mark is ${totalMark}. You have failed.`
-            // `Your final mark is ${totalMark} and your grade is ${grades}. You have failed.`
+            `Your final mark is ${totalMark} and your grade is ${grades}.`
         }
     }
     };
